54467455f92e6f1f1342e38bb2540ccd2f81a4ca
devanondo <dev.abormon28@gmail.com>
2023-10-02 18:19:21 +0600

    [DONE] CREATE CONVERSATION ROUTE

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,50 @@
+import { ChatHeader } from '@/components/chat/chat-header';
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { redirectToSignIn } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
-const ChannleIdPage = () => {
-    return <div>ChannleIdPage</div>;
+interface ChannelIdPageProps {
+    params: {
+        serverId: string;
+        channelId: string;
+    };
+}
+
+const ChannleIdPage = async ({ params }: ChannelIdPageProps) => {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return redirectToSignIn();
+    }
+
+    const channel = await db.channel.findUnique({
+        where: {
+            id: params.channelId,
+        },
+    });
+
+    const member = await db.member.findFirst({
+        where: {
+            profileId: profile.id,
+            serverId: params.serverId,
+        },
+    });
+
+    if (!channel || !member) {
+        redirect('/');
+    }
+
+    return (
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full ">
+            <ChatHeader
+                name={channel.name}
+                serverId={channel.serverId}
+                type="channel"
+            />
+        </div>
+    );
 };
 
 export default ChannleIdPage;
